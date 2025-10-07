90aed3d2e8ca21e18e4f47702588d8da226a59c4
LordSlushii <88372623+LordSlushii@users.noreply.github.com>
2025-10-07 23:57:27 +0530

    Linked actual yt channel

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -6,6 +6,8 @@ import {
   faHandshake,
 } from "@fortawesome/free-regular-svg-icons";
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 const videos = [
   {
@@ -43,7 +45,42 @@ const videos = [
     views: "1.2M views • 2 weeks ago",
   },
 ];
-
+const linkedvideos = [
+  {
+    title:
+      "InspiHER Season 4, Bonus Episode: Tanvi Shah, Founder and Creative Director of Saltie",
+    views: "111 views • 2 months ago",
+    videoid: "NKB6waEMy98",
+  },
+  {
+    title:
+      "InspiHER Season 4, Episode 3: Shubhangi Gupta, GDG Noida Organiser and Women Techmakers Ambassador",
+    views: "263 views • 2 months ago",
+    videoid: "lHDsi5wtj44",
+  },
+  {
+    title:
+      "InspiHER Season 4, Episode 2: Muskan Agarwal, Founder at Cherry Media, ex-Amazon and GHC Scholar",
+    views: "182 views • 2 months ago",
+    videoid: "FNBmFbB6fvE",
+  },
+  {
+    title:
+      "“Computers Have Feelings Too” | Dr. Peter Robinson, University of Cambridge | DSP May 2025",
+    views: "111 views • 4 months ago",
+    videoid: "YEY1P62zAf8",
+  },
+  {
+    title: "Opening Ceremony | Cryptic Hunt 4.0 | graVITas 2025",
+    views: "543 views • 1 month ago",
+    videoid: "tHOMKNF29u4",
+  },
+  {
+    title: "Cryptic Hunt 2024 - The Aftermovie",
+    views: "148 views • 11 months ago",
+    videoid: "FF5XqPccatk",
+  },
+];
 const Page = () => {
   return (
     <div className="relative w-screen h-screen overflow-x-hidden bg-[#181818]">
@@ -102,22 +139,42 @@ const Page = () => {
 
         <h1 className="px-4 font-bold text-xl pt-4">Trending Videos</h1>
         <div className="w-full grid grid-cols-3 grid-rows-2 gap-4 pt-2 p-4">
-          {videos.map((video, index) => (
-            <div key={index} className="h-fit">
-              <div className="w-full h-full bg-white border aspect-video"></div>
+          {linkedvideos.map((video, index) => (
+            <Link
+              href={`https://www.youtube.com/watch?v=${video.videoid}`}
+              key={index}
+              className="h-fit"
+            >
+              <div className="w-full h-full aspect-video">
+                <Image
+                  src={`https://img.youtube.com/vi/${video.videoid}/hqdefault.jpg`}
+                  alt="video thumbnail"
+                  className="w-full h-full object-cover"
+                  width={9999}
+                  height={9999}
+                />
+              </div>
               <div className="flex mt-2 gap-2">
-                <div className="bg-white w-11 h-11 aspect-square rounded-full"></div>
+                <div className="w-11 h-11 aspect-square rounded-full">
+                  <Image
+                    src={`https://yt3.googleusercontent.com/pkzP9B9FarjW5b8gUyOglcUyLBs6UYVuEnkoGYvpgoT3RCNLrgaSwr-Z-TeM287XRt6i8qzZGw=s160-c-k-c0x00ffffff-no-rj`}
+                    alt="video thumbnail"
+                    className="w-full h-full object-cover rounded-full"
+                    width={9999}
+                    height={9999}
+                  />
+                </div>
                 <div>
                   <p className="font-semibold line-clamp-2">{video.title}</p>
                   <p className="text-gray-400 text-sm font-semibold">
-                    {video.channel}
+                    Association for Computing Machinery
                   </p>
                   <p className="text-gray-400 text-sm font-semibold">
                     {video.views}
                   </p>
                 </div>
               </div>
-            </div>
+            </Link>
           ))}
         </div>
       </div>
